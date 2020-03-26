28f0e68b87a61a74a89341d2c659c75688fbea64
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 20:42:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(num)
+  range = (2..num-1).to_a 
+  
+  if num <= 0 || num == 1 
+    return false 
+    
+  elsif range.any? {|i| num % i == 0}
+  return false
+  
+else 
+  return true 
+  
+end
+  
+end
